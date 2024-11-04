1a3238cf681b068d038262a893366ade435a47f1
ay589 <tiffany.feng.25@gmail.com>
2024-11-04 10:36:07 -0500

    chat class changes

--- src/main/java/entity/Chat.java
+++ src/main/java/entity/Chat.java
@@ -1,8 +1,24 @@
 package entity;
+
 import java.util.ArrayList;
 
 public class Chat {
-    private final Arraylist<Message> chatLog = new Arraylist<Message>();
+    private final ArrayList<Message> chatLog = new ArrayList<Message>();
+    private ArrayList<User> users = new ArrayList<User>();
 
-    //public Chat(){}
+    public Chat(ArrayList<User> users) {
+        this.users = users;
+    }
+
+    public void addMessage(Message message) {
+        chatLog.add(message);
+    }
+
+    public ArrayList<Message> getMessages() {
+        return chatLog;
+    }
+
+    public ArrayList<User> getUsers() {
+        return users;
+    }
 }
