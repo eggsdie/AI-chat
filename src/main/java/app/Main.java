3f20b20000c19d2f636c243b5849f08d151a2975
TFeng25 <tiffanyq.feng@mail.utoronto.ca>
2024-11-13 14:14:03 -0500

    flatlaf look and feel

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -1,6 +1,11 @@
 package app;
 
-import javax.swing.JFrame;
+import com.formdev.flatlaf.FlatDarculaLaf;
+import com.formdev.flatlaf.FlatDarkLaf;
+import com.formdev.flatlaf.FlatIntelliJLaf;
+import com.formdev.flatlaf.FlatLightLaf;
+
+import javax.swing.*;
 
 /**
  * The Main class of our application.
@@ -10,8 +15,9 @@ public class Main {
      * Builds and runs the CA architecture of the application.
      * @param args unused arguments
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws UnsupportedLookAndFeelException {
         final AppBuilder appBuilder = new AppBuilder();
+        UIManager.setLookAndFeel(new FlatLightLaf());
         // TODO: add the Logout Use Case to the app using the appBuilder
         final JFrame application = appBuilder
                                             .addLoginView()
