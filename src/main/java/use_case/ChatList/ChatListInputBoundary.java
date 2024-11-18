f3b58927bb5441251084adf588e313631cc5cb38
ay589 <tiffany.feng.25@gmail.com>
2024-11-18 09:49:42 -0500

    delete unused code

--- src/main/java/use_case/ChatList/ChatListInputBoundary.java
+++ src/main/java/use_case/ChatList/ChatListInputBoundary.java
@@ -1,7 +1,5 @@
 package use_case.ChatList;
 
-import use_case.add_friend.AddFriendInputData;
-
 public interface ChatListInputBoundary {
     void addChat(ChatListInputData chatListInputData, String messagePreview);
     boolean removeChat(String name);
