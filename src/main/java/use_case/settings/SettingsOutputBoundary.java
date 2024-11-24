e1a87c8ad34fc0a2f503b227fb12ae86753d48c0
Alisha Pham <alisha.pham@mail.utoronto.ca>
2024-11-24 16:52:03 -0500

    Interface of Settings

---
+++ src/main/java/use_case/settings/SettingsOutputBoundary.java
@@ -0,0 +1,7 @@
+package use_case.settings;
+
+public interface SettingsOutputBoundary {
+    void prepareSuccessView(SettingsOutputData outputData);
+
+    void prepareFailView(String errorMessage);
+}
