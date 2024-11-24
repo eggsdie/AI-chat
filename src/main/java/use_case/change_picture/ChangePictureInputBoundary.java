4fcf23d853a446dced58cf8bf6288de18017f5b0
Alisha Pham <alisha.pham@mail.utoronto.ca>
2024-11-24 16:27:55 -0500

    Interface of ChangePicture

---
+++ src/main/java/use_case/change_picture/ChangePictureInputBoundary.java
@@ -0,0 +1,5 @@
+package use_case.change_picture;
+
+public interface ChangePictureInputBoundary {
+    void execute(ChangePictureInputData changePictureInputData);
+}
---
+++ src/main/java/use_case/change_picture/ChangePictureUserDataAccessInterface.java
@@ -0,0 +1,7 @@
+package use_case.change_picture;
+
+import entity.User;
+
+public interface ChangePictureUserDataAccessInterface {
+    void changePicture(User user);
+}
