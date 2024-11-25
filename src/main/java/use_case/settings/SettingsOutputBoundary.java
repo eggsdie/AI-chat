5d1e99cb7b971c31b88f26a0d19a6be129df31da
Alisha Pham <alisha.pham@mail.utoronto.ca>
2024-11-24 22:36:09 -0500

    Changes by tiffa

--- src/main/java/use_case/settings/SettingsOutputBoundary.java
+++ src/main/java/use_case/settings/SettingsOutputBoundary.java
@@ -4,4 +4,6 @@ public interface SettingsOutputBoundary {
     void prepareSuccessView(SettingsOutputData outputData);
 
     void prepareFailView(String errorMessage);
+
+    void switchToChatListView();
 }
