d3a66d33034d04b42e5d5bb5d48965b7f7ef1939
Alisha Pham <alisha.pham@mail.utoronto.ca>
2024-12-01 05:41:27 -0500

    controller, presenter, output boundary, output data, user DA interface

--- src/main/java/use_case/change_picture/ChangePictureUserDataAccessInterface.java
+++ src/main/java/use_case/change_picture/ChangePictureUserDataAccessInterface.java
@@ -1,9 +1,8 @@
-/*
 package use_case.change_picture;
 
 import entity.User;
 
 public interface ChangePictureUserDataAccessInterface {
     void changePicture(User user);
+    User get(String username);
 }
-*/
