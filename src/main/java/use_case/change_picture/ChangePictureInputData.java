175cef4b66c7fa0afc41b3287ba83123d95ec06f
Alisha Pham <alisha.pham@mail.utoronto.ca>
2024-12-01 11:58:56 -0500

    interactor

--- src/main/java/use_case/change_picture/ChangePictureInputData.java
+++ src/main/java/use_case/change_picture/ChangePictureInputData.java
@@ -17,5 +17,4 @@ public class ChangePictureInputData {
     String getUsername() {
         return username;
     }
-
 }
---
+++ src/main/java/use_case/change_picture/ChangePictureInteractor.java
@@ -0,0 +1,30 @@
+package use_case.change_picture;
+
+import entity.User;
+import entity.UserFactory;
+
+public class ChangePictureInteractor implements ChangePictureInputBoundary {
+    private final ChangePictureUserDataAccessInterface userDataAccessObject;
+    private final ChangePictureOutputBoundary userPresenter;
+    private final UserFactory userFactory;
+
+    public ChangePictureInteractor(ChangePictureUserDataAccessInterface changePictureDataAccessInterface,
+                                    ChangePictureOutputBoundary changePictureOutputBoundary,
+                                    UserFactory userFactory) {
+        this.userDataAccessObject = changePictureDataAccessInterface;
+        this.userPresenter = changePictureOutputBoundary;
+        this.userFactory = userFactory;
+    }
+
+    @Override
+    public void execute(ChangePictureInputData changePictureInputData) {
+        final User user = userFactory.create(changePictureInputData.getUsername(),
+                userDataAccessObject.get(changePictureInputData.getUsername()).getEmail(),
+                userDataAccessObject.get(changePictureInputData.getUsername()).getPassword());
+
+        userDataAccessObject.changePicture(user);
+
+        final ChangePictureOutputData changePictureOutputData = new ChangePictureOutputData(user.getName(), false);
+        userPresenter.prepareSuccessView(changePictureOutputData);
+    }
+}
